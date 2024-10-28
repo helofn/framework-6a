0660d5815d6e2a4deff2419f8167c72046d9d3ec
helofn <120032614+helofn@users.noreply.github.com>
2024-10-28 09:51:59 -0300

    Add files via upload

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,26 +1,13 @@
+import { Container } from "react-bootstrap";
 import Footer from "./modulos/footer";
-import Navbar from "./modulos/navbar";
-    import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-
+import Menu from "./modulos/navbar";
 
 export default function Index() {
   return <div>
-    <Navbar />
-    Bem Vindos
+    <Menu />
     <Container>
-      <Row>
-        <Col className= "bg-dark1"> of 2</Col>
-        <Col>2 of 2</Col>
-      </Row>
-      <Row>
-        <Col>1 of 3</Col>
-        <Col>2 of 3</Col>
-        <Col>3 of 3</Col>
-      </Row>
+      Bem Vindos a disciplina 
     </Container>
     <Footer />
-
   </div>;
 }
